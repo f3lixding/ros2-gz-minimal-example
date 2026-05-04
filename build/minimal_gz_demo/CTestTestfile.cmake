35af8dd1206ed7e3b26b5af31d3a94b6c70176a6
Felix Ding <felixding1989@gmail.com>
2026-05-03 22:11:00 -0700

    updates gitignore

--- build/minimal_gz_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/felix/doodle/minimal_gz_ros2/ros_ws/src/minimal_gz_demo
-# Build directory: /home/felix/doodle/minimal_gz_ros2/build/minimal_gz_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
